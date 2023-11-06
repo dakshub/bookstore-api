828f284c2bc51410fa348ffd088be41055ab5d83
daksh <45903885+dakshub@users.noreply.github.com>
2023-11-05 22:40:30 -0500

    Add validation rules for POST book request

---
+++ app/Http/Requests/PostBookRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostBookRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'isbn' => 'required|string|min:13|max:13|unique:books',
+            'title' => 'required|string|min:1',
+            'description' => 'required|string|min:1',
+            'authors' => 'required|array|min:1',
+            'authors.*' => 'required|integer|exists:authors,id',
+            'published_year' => 'required|integer|min:1900|max:2020'
+        ];
+    }
+}
