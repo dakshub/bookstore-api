2373385d49577022754813afa49ae92504e21a9b
daksh <45903885+dakshub@users.noreply.github.com>
2023-11-05 22:21:56 -0500

    Add Book model

---
+++ app/Book.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Book extends Model
+{
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'isbn',
+        'title',
+        'description',
+        'published_year'
+    ];
+
+    protected $with = ['authors'];
+
+    public function authors()
+    {
+        return $this->belongsToMany(Author::class, 'book_author');
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(BookReview::class);
+    }
+}
